2b5f3a3271a67cac8573ae5422468001eaa4e100
mffn-rscl <mffnrscl@gmail.com>
2026-09-08 18:24:58 +0300

    added some buttons, which allows to change bpm value/ interrupt pendings configured

--- Inc/tim2_bpm_clock_control.h
+++ Inc/tim2_bpm_clock_control.h
@@ -4,6 +4,19 @@
 #define TIM2_PSC_DIVIDER 7200U
 #define TIM2_TICK_PER_SEC 600000U
 
-void tim2_init(uint16_t initial_bpm);
+#define AFIO_EXTI_BUTTONS_MASK 0x00U
+#define GPIOA_CRH_BUTTON_CONFIGURATION (0x88U << 8)
+#define EXTI_PA8_PIN   (0x1U << 8)
+#define EXTI_PA9_PIN   (0x1U << 9)
+
+#define MAX_BPM_VALUE 250U
+#define MIN_BPM_VALUE 30U
+
+void tim2_init(void);
+
+
+void button_regulation_init(void);
+
+
 
 #endif /*TIM2_BPM_CLOCK_CONTROL*/ 
--- Src/tim2_bpm_clock_control.c
+++ Src/tim2_bpm_clock_control.c
@@ -1,13 +1,14 @@
 #include "../Inc/tim2_bpm_clock_control.h"
 
+volatile static uint8_t current_bpm = 60; 
 
-void tim2_init(uint16_t initial_bpm)
+void tim2_init(void)
 {
     RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 
     TIM2->PSC = TIM2_PSC_DIVIDER - 1;
 
-    TIM2->ARR = ((TIM2_TICK_PER_SEC + (initial_bpm / 2)) / initial_bpm) - 1;
+    TIM2->ARR = ((TIM2_TICK_PER_SEC + (current_bpm / 2)) / current_bpm) - 1;
 
     TIM2->EGR |= TIM_EGR_UG;
 
@@ -20,6 +21,51 @@ void tim2_init(uint16_t initial_bpm)
 
     TIM2->CR1 |= TIM_CR1_CEN;
 }
+
+
+void button_regulation_init(void)
+{
+  RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN;
+  
+  GPIOA->CRH &= ~(GPIO_CRH_CNF9 | GPIO_CRH_CNF8);
+  /*enabling the buttons*/
+  GPIOA->CRH |= GPIOA_CRH_BUTTON_CONFIGURATION;
+  GPIOA->ODR |= EXTI_PA8_PIN | EXTI_PA9_PIN;
+
+  AFIO->EXTICR[2] &= ~(0xFFU);
+  
+  EXTI->IMR |= EXTI_PA8_PIN | EXTI_PA9_PIN;
+  EXTI->FTSR |= EXTI_PA8_PIN | EXTI_PA9_PIN;
+  EXTI->RTSR &= ~(EXTI_PA8_PIN | EXTI_PA9_PIN);
+  EXTI->PR =  EXTI_PA8_PIN | EXTI_PA9_PIN;
+  
+  NVIC_SetPriority(EXTI9_5_IRQn, 2);
+  NVIC_EnableIRQ(EXTI9_5_IRQn);
+}
+
+void EXTI9_5_IRQ_handler(void)
+{
+  if(EXTI->PR & EXTI_PA8_PIN)
+  {
+     EXTI->PR = EXTI_PA8_PIN;
+     if(current_bpm + 1 <= MAX_BPM_VALUE)
+     {
+      current_bpm++;
+     }
+
+  }
+  else if(EXTI->PR & EXTI_PA9_PIN)
+  {
+    EXTI->PR = EXTI_PA9_PIN;
+     if(current_bpm - 1 >= MIN_BPM_VALUE)
+     {
+      current_bpm--;
+     }
+
+  }
+  TIM2->ARR = ((TIM2_TICK_PER_SEC + (current_bpm / 2)) / current_bpm) - 1;
+}
+
 
 void TIM2_IRQ_handler(void)
 {
